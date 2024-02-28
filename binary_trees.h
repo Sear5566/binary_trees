9b98f5be7549c271988cf2a93715eac51d921d1e
SoladrinHub <cointoken8021@gmail.com>
2024-02-28 07:54:43 -0800

    header file

--- binary_trees.h
+++ binary_trees.h
@@ -1,5 +1,6 @@
 #ifndef MAIN_H
-#define MAIN_h
-binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+#define MAIN_H
 #include <stdlib.h>
+#include <stdio.h>
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 #endif
