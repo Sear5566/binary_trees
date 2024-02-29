f057ee1c5ae6fd48715044400aee908ad193583c
SoladrinHub <cointoken8021@gmail.com>
2024-02-29 05:17:11 -0800

    header file

--- binary_trees.h
+++ binary_trees.h
@@ -1,6 +1,27 @@
-#ifndef MAIN_H
-#define MAIN_H
-#include <stdlib.h>
+#ifndef BINARY_TREE_H
+#define BINARY_TREE_H
 #include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * struct binary_tree_s - Binary tree node
+ *
+ * @n: Integer stored in node
+ * @parent: Pointer to the parent node
+ * @left: Pointer to the left child node
+ * @right: Pointer to the right child node
+ */
+struct binary_tree_s
+{
+	int n;
+	struct binary_tree_s *parent;
+	struct binary_tree_s *left;
+	struct binary_tree_s *right;
+};
+typedef struct binary_tree_s binary_tree_t;
+
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
-#endif
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
+void binary_tree_print(const binary_tree_t *);
+#endif /*BINARY_TREE_H*/
